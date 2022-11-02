151829c2cc50c61d7da57d800455293d07f47d6a
Sam Hindess <mail@sohcah.dev>
2022-11-02 21:15:37 +0000

    API: Generic Clan Fallbacks

--- api/utils/cacher.ts
+++ api/utils/cacher.ts
@@ -1,5 +1,27 @@
 import fjss from "fast-json-stable-stringify";
 
+export async function tryWithEach<T, R>(items: T[], fn: (item: T) => Promise<R>) {
+  for (const item of items) {
+    try {
+      return await fn(item);
+      // eslint-disable-next-line no-empty
+    } catch (e) {}
+  }
+  throw new Error("No valid try");
+}
+
+const GENERIC_CLANS = [
+  2041, // Cup of Dev Clan,
+  1349, // Cup of Coffee Clan
+  1, // Munzee HQ
+  // First 100 clans
+  ...new Array(100).fill(0).map((_, i) => i + 2),
+];
+
+export async function tryWithEachGenericClan<R>(fn: (clan_id: number) => Promise<R>) {
+  return await tryWithEach(GENERIC_CLANS, fn);
+}
+
 export class Cacher<TData, TInput = string | undefined> {
   private cache: Map<string, [Promise<TData>, number]> = new Map();
 
